34b669e764e5f66e530431688e174afd5e92f63b
Robert Isaacs <robertisaacs87@yahoo.com>
2021-12-13 17:00:02 -0600

    file restructure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 //https://api.openweathermap.org/data/2.5/weather?q=&appid=d778222891cde7aa699d4d39200e67b4
 var button = document.querySelector(".button");
 
-var name = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
-
 button.addEventListener("click", function() {
 var inputValue = document.querySelector(".inputValue");
 var key = "d778222891cde7aa699d4d39200e67b4";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + key
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + "&appid=" + key;
+
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                var nameValue = data['name'];
+                var tempValue = data['list']['0']['main']['temp']; 
+                var descValue = data['weather'][0]['description'];
+
+                name.innerHTML = nameValue;
+                temp.innerHTML = tempValue;
+                desc.innerHTML = descValue;
                 })
             }else {
                 alert("Error");
@@ -25,19 +32,4 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.v
     });
 })
 
-// let userInput;
 
-// function setup() {
-//     userInput = select("#userinput");
-//     userInput.changed(openWeather)
-//     openWeather();
-
-//     function openWeather() {
-//         let term = userInput.value();
-//          console.log(term)
-//     }
-// }
-
-// function weatherBalloon(  ) {
-    
-// }
